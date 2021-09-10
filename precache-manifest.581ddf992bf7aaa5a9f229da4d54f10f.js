4f1b84c9459108f7caf2f890ac05591767e265a5
Mark Fang <mark.fang.mo@gmail.com>
2021-09-09 22:20:10 -0500

    Updates

---
+++ precache-manifest.581ddf992bf7aaa5a9f229da4d54f10f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc59816b1e258da61a2783f946c250c",
+    "url": "/rubiks-timer/index.html"
+  },
+  {
+    "revision": "47f0fc28ffd5fea0c12a",
+    "url": "/rubiks-timer/static/css/main.96eedf2b.chunk.css"
+  },
+  {
+    "revision": "d029817a67ef124cf8af",
+    "url": "/rubiks-timer/static/js/2.07bea225.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/rubiks-timer/static/js/2.07bea225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f0fc28ffd5fea0c12a",
+    "url": "/rubiks-timer/static/js/main.f6b17fa7.chunk.js"
+  },
+  {
+    "revision": "21cf184268c6e58ff019",
+    "url": "/rubiks-timer/static/js/runtime-main.d8a4ac64.js"
+  }
+]);
